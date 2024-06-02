ca9bbd299bc9bf2edb47bc47980775fd91b732b0
Ben Overmyer <ben@overmyer.net>
2024-06-02 06:54:07 -0400

    Add cultures, towns, and regions to name types

--- src/fantasy_names.ts
+++ src/fantasy_names.ts
@@ -1,40 +1,18 @@
 import * as MUN from "@ironarachne/made-up-names";
 import NameResponse from "./name_response.js";
 
-const dragonbornGenSet: MUN.GeneratorSet = new MUN.DragonbornSet();
-const dwarfGenSet: MUN.GeneratorSet = new MUN.DwarfSet();
-const elfGenSet: MUN.GeneratorSet = new MUN.ElfSet();
-const gnomeGenSet: MUN.GeneratorSet = new MUN.GnomeSet();
-const goblinGenSet: MUN.GeneratorSet = new MUN.GoblinSet();
-const halfElfGenSet: MUN.GeneratorSet = new MUN.HalfElfSet();
-const halflingGenSet: MUN.GeneratorSet = new MUN.HalflingSet();
-const halfOrcGenSet: MUN.GeneratorSet = new MUN.HalfOrcSet();
-const humanGenSet: MUN.GeneratorSet = new MUN.HumanSet();
-const orcGenSet: MUN.GeneratorSet = new MUN.OrcSet();
-const tieflingGenSet: MUN.GeneratorSet = new MUN.TieflingSet();
-const trollGenSet: MUN.GeneratorSet = new MUN.TrollSet();
+const generatorSets: MUN.GeneratorSet[] = MUN.fantasyRaceSets();
 
-const nameGeneratorSets: Record<string, MUN.GeneratorSet> = {
-  dragonborn: dragonbornGenSet,
-  dwarf: dwarfGenSet,
-  elf: elfGenSet,
-  gnome: gnomeGenSet,
-  goblin: goblinGenSet,
-  halfling: halflingGenSet,
-  halfelf: halfElfGenSet,
-  halforc: halfOrcGenSet,
-  human: humanGenSet,
-  orc: orcGenSet,
-  tiefling: tieflingGenSet,
-  troll: trollGenSet,
-};
-
-export function getNames(race: string, nameType: string, count: number): NameResponse {
+export function getNames(
+  race: string,
+  nameType: string,
+  count: number,
+): NameResponse {
   if (count < 1) {
     throw new Error(`invalid count: ${count}`);
   }
 
-  let genSet = nameGeneratorSets[race];
+  let genSet = MUN.getSetByName(race, generatorSets);
   let result: NameResponse = {
     count: count,
     names: [],
@@ -46,6 +24,12 @@ export function getNames(race: string, nameType: string, count: number): NameRes
     result.names = genSet.female.generate(count);
   } else if (nameType == "family" && genSet.family) {
     result.names = genSet.family.generate(count);
+  } else if (nameType == "region" && genSet.country) {
+    result.names = genSet.country.generate(count);
+  } else if (nameType == "town" && genSet.town) {
+    result.names = genSet.town.generate(count);
+  } else if (nameType == "culture" && genSet.culture) {
+    result.names = genSet.culture.generate(count);
   } else {
     throw new Error(`bad name type: ${nameType}`);
   }
--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,10 @@ import { getNames } from "./fantasy_names.js";
 const app = new Hono();
 app.use(logger());
 
-app.get(
-  "/",
-  (c) =>
-    c.text(
-      "This is MUNA. Use it like /race/elf/female/10. The following races are available: dragonborn, dwarf, elf, gnome, goblin, halfling, halfelf, halforc, human, orc, tiefling, troll.",
-    ),
+app.get("/", (c) =>
+  c.text(
+    "This is MUNA. It takes the form /race/:race_name/:name_type/:count. Use it like /race/elf/female/10. The following races are available: dragonborn, dwarf, elf, gnome, goblin, halfling, halfelf, halforc, human, orc, tiefling, troll. The following name types are available: male, female, family, region, town.",
+  ),
 );
 
 app.get("/race/:race/:nameType/:count", (c) => {
