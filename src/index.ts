6f9c436b1d741ef4f99379cfdb0b1213dcb21105
Ben Overmyer <ben@overmyer.net>
2025-01-19 07:31:24 -0500

    Fix race name documentation and make it dynamic

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,20 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
 import { getNames } from "./fantasy_names.js";
+import * as MUN from "@ironarachne/made-up-names";
+
+const generatorSets: MUN.GeneratorSet[] = MUN.fantasyRaceSets();
 
 const app = new Hono();
 app.use(logger());
 
-app.get("/", (c) =>
-  c.text(
-    "This is MUNA. It takes the form /race/:race_name/:name_type/:count. Use it like /race/elf/female/10. The following races are available: dragonborn, dwarf, elf, gnome, goblin, halfling, halfelf, halforc, human, orc, tiefling, troll. The following name types are available: male, female, family, given, region, town.",
-  ));
+app.get("/", (c) => {
+  const raceNames = generatorSets.map((set) => set.name);
+
+  return c.text(
+    `This is MUNA. It takes the form /race/:race_name/:name_type/:count. Use it like /race/elf/female/10. The following races are available: ${raceNames}. The following name types are available: male, female, family, given, region, town.`,
+  );
+});  
 
 app.get("/race/:race/:nameType/:count", (c) => {
   const { race, nameType, count } = c.req.param();
